99e7872d6ad4a752afc44559dd9db7e7e2a2095d
Eldon Williams <eldon.ew.com@gmail.com>
2023-03-29 08:48:59 -0500

    midpoint and documentation

--- Assets/Scripts/util/Vector3Utils.cs
+++ Assets/Scripts/util/Vector3Utils.cs
@@ -12,8 +12,14 @@ public class Vector3Utils {
         );
     }
 
+    // Removes the y component of a Vector3 and normalizes it
     public static Vector3 ProjectHorizontally(Vector3 a) {
         a.y = 0;
         return a.normalized;
+    }
+
+    // Finds the midpoint of two Vector3s
+    public static Vector3 Midpoint(Vector3 a, Vector3 b) {
+        return Vector3.Lerp(a, b, 0.5f);
     }
 }
