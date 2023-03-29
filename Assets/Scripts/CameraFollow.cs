b23e89b8a4f149801ef8008114c9ca1b97ed9646
Eldon Williams <eldon.ew.com@gmail.com>
2023-03-29 08:40:09 -0500

    stable save

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,14 +13,14 @@ public class CameraFollow : MonoBehaviour
         STATIONARY,
     }
 
-    public float move = 5.0f;
     // The type of following currently active
     public FollowType followType = FollowType.MOVING;
+    // The currently tracked object
     public Transform trackedObject;
+    // The offset in the +Y direction
     public float offsetY = 2.0f;
+    // The speed to track the object at
     public float trackSpeed = 2.0f;
-
-    private Transform cameraTransform;
 
     /*
         FOR FollowType.STATIONARY
@@ -38,10 +38,9 @@ public class CameraFollow : MonoBehaviour
 
     void Start()
     {
-        cameraTransform = gameObject.GetComponent<Camera>().transform;
         var (position, rotation) = calculateCameraPosAndRot();
-        cameraTransform.position = position;
-        cameraTransform.rotation = Quaternion.Euler(rotation);
+        transform.position = position;
+        transform.rotation = Quaternion.Euler(rotation);
     }
 
     void Update()
@@ -59,58 +58,104 @@ public class CameraFollow : MonoBehaviour
         }
     }
 
-    Vector3 lastPosition = Vector3.zero;
-    float rotLerpMulti = 0.75f;
-    bool isTrackedVisible;
-
     private void movingUpdate() {
-        /*
-            On first update, lastPosition will be Vector3.zero.
-                Set to trackedObject.position, b/c deltaPosition on first frame should be ~0,0,0
-            Find the change in position (newPos - oldPos)
-            Update last position for next frame
-        */
-        if (lastPosition == Vector3.zero) lastPosition = trackedObject.position;
-        Vector3 deltaPosition = trackedObject.position - lastPosition;
-        lastPosition = trackedObject.position;
-        // The deltaVector but with a magnitude of 1, used for adding to other vectors
-        // trackedObject.position where y compoonent is 0
+        // This is the goal position of the camera
+        Vector3 position;
+        // This is the goal rotation of the camera
+        Quaternion rotation;
 
-        isTrackedVisible = trackedObject.GetComponent<Renderer>().isVisible;
-
-        //
-        Vector3 position = trackedObject.position - (deltaPosition * move / Time.deltaTime);
-        // position -= trackedObject.forward;
-        // Position calculation
         /*
-            Position is the trackedObject's position subtracted by the change in position ()
+            This is where we get the mouse's screen position as a world position
+            If it is not hitting an object within 30 units, it will not pick up anything.
+
+            The midpoint is the midpoint between the mouse's 3d position and the target
+            If no mouse exists, it will be the midpoint between the target's forward vector,
+            and a circle of configurable radius.
         */
-        // Removed: position -= trackedObject.forward;
+        // Vector3 mousePos = Input.mousePosition;
+        // Ray mouseRay = transform.GetComponent<Camera>().ScreenPointToRay(mousePos);
+        // RaycastHit mouseRaycastHit;
+        // // The midpoint between trackedObject.position and mouseRaycastHit.point
+        // Vector3 midpoint;
+        // // The radius of the circle made by trackedObject.position and mouseRaycastHit.point
+        // float radius;
+        // if (Physics.Raycast(mouseRay, out mouseRaycastHit, 30.0f)) {
+        //     Vector3 point = mouseRaycastHit.point;
+        //     point.y = 0;
+        //     midpoint = Vector3Utils.Midpoint(trackedObject.position, point);
+        //     midpoint = Vector3.ClampMagnitude(midpoint, 3.0f);
+        //     // radius of circle formed by two points
+        //     radius = midpoint.magnitude * 2;
+        // } else {
+        //     midpoint = trackedObject.position + new Vector3(1, 0, 1);
+        //     radius = midpoint.magnitude * 2;
+        // }
+        // midpoint.y = 0;
+
+        position = trackedObject.position;
+        // Move back by radius, putting this as a point on cylinder where h=inf
+        position -= trackedObject.forward;
         position.y += offsetY;
-        position.y -= deltaPosition.magnitude * move / Time.deltaTime;
-        Vector3 rotation = Quaternion.LookRotation(((deltaPosition / Time.deltaTime + trackedObject.position) - position).normalized).eulerAngles;
-        //
-        /*
-            Use the deltaVector 
-        */
 
-        rotLerpMulti = Mathf.Lerp(rotLerpMulti, deltaPosition.magnitude > 0 ? 0.0f : 0.75f, Time.deltaTime * trackSpeed);
-        /*
-            Rotation stops when moving b/c it can lead to weird movement errors.
-            Lerp is used because of the nice cinematic effect it creates.
-        */
-        //
-        Vector3 newPosition = Vector3.Lerp(cameraTransform.position, position, Time.deltaTime * trackSpeed);
-        newPosition.y = Mathf.Lerp(cameraTransform.position.y, position.y, Time.deltaTime * trackSpeed);
-        cameraTransform.position = newPosition;
-        
-        //
-        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(rotation), Time.deltaTime * trackSpeed * rotLerpMulti);
+        rotation = Quaternion.LookRotation(trackedObject.position - transform.position);
+
+        transform.position = Vector3.Lerp(transform.position, position, trackSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, trackSpeed * Time.deltaTime);
     }
 
     private void stationaryUpdate() {
         var (position, rotation) = calculateCameraPosAndRot();
-        cameraTransform.position = Vector3.Lerp(cameraTransform.position, position, Time.deltaTime * trackSpeed);
-        cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(rotation), Time.deltaTime * trackSpeed);
+        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * trackSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotation), Time.deltaTime * trackSpeed);
     }
+
+    // The Old Moving Update
+    // private void movingUpdate() {
+    //     /*
+    //         On first update, lastPosition will be Vector3.zero.
+    //             Set to trackedObject.position, b/c deltaPosition on first frame should be ~0,0,0
+    //         Find the change in position (newPos - oldPos)
+    //         Update last position for next frame
+    //     */
+    //     if (lastPosition == Vector3.zero) lastPosition = trackedObject.position;
+    //     Vector3 deltaPosition = trackedObject.position - lastPosition;
+    //     deltaPosition.y = 0;
+    //     lastPosition = trackedObject.position;
+    //     // The deltaVector but with a magnitude of 1, used for adding to other vectors
+    //     Vector3 deltaVector = deltaPosition / Time.deltaTime;
+    //     deltaVector.y = 0;
+        
+    //     // trackedObject.position where y compoonent is 0
+    //     Vector3 noYPosition = trackedObject.position;
+    //     noYPosition.y = 0;
+
+    //     // Position calculation
+    //     /*
+    //         Position is the trackedObject's position subtracted by the change in position ()
+    //     */
+    //     Vector3 position = trackedObject.position;
+    //     position -= Vector3Utils.ProjectHorizontally(trackedObject.forward) * 5;
+    //     // Removed: position -= trackedObject.forward;
+    //     position.y += offsetY;
+    //     position.y -= Mathf.Clamp(deltaVector.magnitude, 0, offsetY/4);
+    //     /*
+    //         Use the deltaVector 
+    //     */
+    //     Vector3 rotation = Quaternion.LookRotation((deltaVector + noYPosition - position).normalized).eulerAngles;
+
+    //     /*
+    //         Rotation stops when moving b/c it can lead to weird movement errors.
+    //         Lerp is used because of the nice cinematic effect it creates.
+    //     */
+    //     rotationLerpSave = Mathf.Lerp(
+    //         rotationLerpSave,
+    //         deltaPosition.magnitude > 0 ? 0.0f : 0.75f,
+    //         Time.deltaTime);
+    //     //
+    //     Vector3 newPosition = Vector3.Lerp(cameraTransform.position, position, Time.deltaTime * trackSpeed);
+    //     newPosition.y = Mathf.Lerp(cameraTransform.position.y, position.y, Time.deltaTime * trackSpeed);
+    //     cameraTransform.position = newPosition;
+
+    //     cameraTransform.rotation = Quaternion.Lerp(cameraTransform.rotation, Quaternion.Euler(rotation), Time.deltaTime * trackSpeed * rotationLerpSave);
+    // }
 }
