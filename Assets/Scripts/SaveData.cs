cee0ecf464c30a7cbd0213c1fcdc9ba3ded2e973
Eldon Williams <eldon.ew.com@gmail.com>
2023-04-18 08:32:20 -0500

    completed first draft of save system, testing

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -1,16 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 
 [System.Serializable]
 public class SaveData
 {
+    static bool SaveAlreadyMade = false;
+    /// <summary>
+    ///  A quick helper function to load a save from a file.
+    ///  You shouldn't have two Save's active at the same time for the same path.
+    ///  Just saving you some future headache, future programmer
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static SaveData GetSaveFromFile(string path) {
+        //
+        if (SaveAlreadyMade) {
+            Debug.Log("Two SaveData instances have been made from GetSaveFromFile, this warning is just incase this means something bad.");
+        }
+        SaveAlreadyMade = true;
+        //
+        SaveData sd = new SaveData(path);
+        sd.Load();
+        return sd;
+    }
+
+    /// <summary>
+    ///  How much alien dna the player has
+    ///  Used for player upgrades
+    /// </summary>
+    public int alienDNA;
+
+    /// <summary>
+    ///  The path, relative to persistentDataPath this save is located at
+    /// </summary>
+    [System.NonSerialized]
+    public string path;
+
+    /// <summary>
+    ///  The full path of the save, cannot be changed.
+    ///  Is recalculated every time and I'm not sure of the time complexity of the function
+    /// </summary>
+    /// <returns></returns>
+    public string fullPath
+    { get => Path.Combine(Application.persistentDataPath, path); }
+
+    public SaveData(string path) {
+        this.path = path;
+    }
+
+    /// <summary>
+    ///  Saves the data to the path of the SaveData.
+    /// </summary>
+    /// <returns>Was the operation successful?</returns>
+    public bool Save()
+    {
+        try
+        {
+            File.WriteAllText(fullPath, ToJson());
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///  Loads the data from the path of the SaveData. Will overwrite any new changes if already loaded.
+    /// </summary>
+    /// <returns>Was the operation successful?</returns>
+    public bool Load()
+    {
+        try
+        {
+            string contents = File.ReadAllText(fullPath);
+            LoadFromJson(contents);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    ///  Converts the object to a JSON representation
+    /// </summary>
+    /// <returns>The JSON version of the save</returns>
     public string ToJson()
     {
         return JsonUtility.ToJson(this);
     }
 
-    public void LoadFromJson(string jsonString) {
+    /// <summary>
+    ///  Inserts data into the SaveData object from the json string
+    /// </summary>
+    /// <param name="jsonString">The string containing the json</param>
+    public void LoadFromJson(string jsonString)
+    {
         JsonUtility.FromJsonOverwrite(jsonString, this);
     }
 }
---
+++ Assets/Scripts/SaveDataManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDataManager : MonoBehaviour
+{
+    /// <summary>
+    ///  Memo of the SaveDataManager
+    /// </summary>
+    static SaveDataManager sdm;
+
+    /// <summary>
+    ///  Get the SaveDataManager
+    /// </summary>
+    /// <returns></returns>
+    public static SaveDataManager GetSaveDataManager() {
+        if (sdm) return sdm;
+        sdm = UnityUtil.GetRootComponent<SaveDataManager>();
+        return sdm;
+    }
+
+    /// <summary>
+    ///  Get the SaveData from the SaveDataManager
+    /// </summary>
+    /// <returns></returns>
+    public static SaveData GetSaveData() {
+        if (sdm) return sdm._save;
+        return GetSaveDataManager()._save;
+    }
+
+    /// <summary>
+    ///  The path of the save managed by the SaveDataManager
+    /// </summary>
+    [Tooltip("The path of the save")]
+    public string SavePath = "Save01.dat";
+
+    /// <summary>
+    ///  The amount of time that must elapse between saves, to prevent spam saves
+    /// </summary>
+    [Tooltip("The amount of time that must elapse between saves, to prevent spam saves")]
+    public float MaxSaveInterval = 0.2f;
+
+    private SaveData _save;
+
+    private float _lastSave;
+
+    void Start() {
+        _save = SaveData.GetSaveFromFile(SavePath);
+    }
+
+    void OnApplicationQuit() {
+        Save();
+    }
+
+    void OnApplicationFocus(bool hasFocus) {
+        if (hasFocus) return;
+        Save();
+    }
+
+    private void Save() {
+        if (_lastSave + MaxSaveInterval > Time.time) return;
+
+        if (_save.Save()) {
+            Debug.Log("Saved!");
+        } else {
+            Debug.Log("Save Fail :(");
+        }
+
+        _lastSave = Time.time;
+    }
+}
